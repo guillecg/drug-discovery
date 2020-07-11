42f7d1e60305a544ee1575022916af83d733e9cc
guillecg <guillemcg92@gmail.com>
2020-07-12 00:12:13 +0200

    Add docstring to create_test_data.py

--- test/data/create_test_data.py
+++ test/data/create_test_data.py
@@ -4,67 +4,70 @@ from rdkit.Chem import PandasTools
 from modules.data.data_loaders import DataLoaderManager
 
 
-def create_test_data(path: str):
+def create_test_data(path: str, n_rows: int = 10) -> None:
+    """Create test data files for all `DataLoaderManager` supported formats
+    from a given data file. The resulting files only contain the first `n_rows`
+    to avoid overhead in tests.
+
+    Parameters
+    ----------
+    path : str
+        Path to the source file from which all test data files will be
+        generated.
+
+    n_rows : int, default=10
+        Number of maximum rows to retrieve in order to avoid overhead in tests.
+    """
 
     data_loader = DataLoaderManager()
     data = data_loader.load(path=path)
 
-    # Get only the first 10 records to avoid overhead in tests
-    data = data[:10]
+    # Get only the first `n_rows` records to avoid overhead in tests
+    data = data[:n_rows]
 
     # CSV
-    path = 'test/data/test_default.csv'
-
+    path = 'test/data/test_data.csv'
     data.to_csv(
         path,
         header=True,
         index=False
     )
-
     assert len(data_loader.load(path=path)) == 10
 
     # Excel (.xls)
-    path = 'test/data/test_default.xls'
-
+    path = 'test/data/test_data.xls'
     data.to_excel(
         path,
         header=True,
         index=False
     )
-
     assert len(data_loader.load(path=path)) == 10
 
     # Excel (.xlsx)
-    path = 'test/data/test_default.xlsx'
-
+    path = 'test/data/test_data.xlsx'
     data.to_excel(
         path,
         header=True,
         index=False
     )
-
     assert len(data_loader.load(path=path)) == 10
 
     # SMILES
-    path = 'test/data/test_default.smi'
-
+    path = 'test/data/test_data.smi'
     w = Chem.SmilesWriter(path)
     for smi in data['Molecule']:
         w.write(smi)
     w.close()
-
     assert len(data_loader.load(path=path)) == 10
 
     # SDF
-    path = 'test/data/test_default.sdf'
-
+    path = 'test/data/test_data.sdf'
     PandasTools.WriteSDF(
         data,
         path,
         molColName='Molecule',
         properties=data.columns.tolist()
     )
-
     assert len(data_loader.load(path=path)) == 10
 
 
