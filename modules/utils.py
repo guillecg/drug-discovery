7ad8fa944ceadd80851e92ac0a6671e74f4add69
guillecg <guillemcg92@gmail.com>
2020-07-11 20:54:13 +0200

    Add type hinting in utils.py and test_utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,10 +1,12 @@
 import os
 import glob
 
+from typing import List
+
 import yaml
 
 
-def load_yaml(path: str):
+def load_yaml(path: str) -> dict:
     """Auxiliary function for loading YAMLs."""
 
     with open(path) as file:
@@ -13,6 +15,7 @@ def load_yaml(path: str):
         return yaml.load(file, Loader=yaml.FullLoader)
 
 
-def get_test_data_paths(data_dir: str, data_type: str) -> list:
-    """Auxiliary function for getting test data paths."""
+def get_test_data_paths(data_dir: str, data_type: str) -> List[str]:
+    """Auxiliary function for retrieving test data paths."""
+
     return glob.glob(os.path.join(data_dir, f'*{data_type}'))
--- test/test_utils.py
+++ test/test_utils.py
@@ -4,32 +4,35 @@ from modules.data.data_loaders import DataLoaderManager
 from modules.utils import load_yaml, get_test_data_paths
 
 
-# IMPORTANT NOTE: all test files contain, by default, the 10 first records in
-# 'data/Tox21/tox21_10k_data_all.sdf'
-CONFIG = load_yaml(path='config.yaml')
-TEST_DATA_DIR = CONFIG.get('TEST_DATA_DIR')
+@pytest.mark.parametrize('path', ['config.yml'])
+def test_load_yaml(path: str) -> None:
+    """This test fails if the loaded file is not a dict."""
 
-
-@pytest.fixture(scope='module')
-def data_loader_manager():
-    return DataLoaderManager()
-
-
-@pytest.mark.parametrize('path', ['config.yaml'])
-def test_load_yaml(path):
-    """This test fails if the result returned is not a dict."""
     yaml_file = load_yaml(path=path)
 
     assert isinstance(yaml_file, dict)
 
 
-def test_get_test_data_paths(data_loader_manager):
-    """This test fails if no test file with the required format is found."""
+def test_get_test_data_paths(
+        data_loader_manager: DataLoaderManager,
+        config_path: str
+) -> None:
+    """This test fails if:
+        - The returned paths are not a list
+        - No test file with the required format is found
+        - Any element of the list is not a string
+    """
+
+    # Get test data directory from test config
+    config = load_yaml(path=config_path)
+    test_data_dir = config.get('TEST_DATA_DIR')
+
     for data_type in data_loader_manager.data_loaders.keys():
         paths = get_test_data_paths(
-            data_dir=TEST_DATA_DIR,
+            data_dir=test_data_dir,
             data_type=data_type
         )
 
-        assert paths
         assert isinstance(paths, list)
+        assert len(paths)
+        assert all([isinstance(path, str) for path in paths])
